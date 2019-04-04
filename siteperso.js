fae01486278cde346c11c0c51ad309ff8f1ee562
Nica <jacqueline.nguyen.41@gmail.com>
2019-04-04 15:53:52 +0200

    My website

---
+++ siteperso.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.slick-codepen').slick({
+    draggable: true,
+    accessibility: false,
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 1,
+    arrows: true,
+    dots: true,
+    swipeToSlide: true,
+    infinite: false
+  });
+});
+
